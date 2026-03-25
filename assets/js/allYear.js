028ad42e8f061700b331e2923124f0826913a1d5
Sandeep kumar <skverma895746@gmail.com>
2026-03-26 00:30:08 +0530

    Update allYear.js

--- assets/js/allYear.js
+++ assets/js/allYear.js
@@ -5,7 +5,7 @@ if (links && errorBox) {
         link.addEventListener("click", (e) => {
             if (link.getAttribute("href") === "" ||link.getAttribute("href")=="#") {
                 e.preventDefault();
-                errorBox.textContent = "⚠️PDF will available soon."
+                errorBox.textContent = "⚠️ PDF will available soon."
                 errorBox.style.display = "flex";
             }
             setTimeout(() => {
