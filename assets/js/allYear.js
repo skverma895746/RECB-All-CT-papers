d59cbbe120d5656ff93a8294535abe885269b948
unknown <jigyasuraj895746@gmail.comgit config --global user.name jigyasu-raj>
2026-04-26 23:59:25 +0530

    pupbox updated

--- assets/js/allYear.js
+++ assets/js/allYear.js
@@ -3,18 +3,21 @@ const links = document.querySelectorAll("a")
 if (links && errorBox) {
     links.forEach((link) => {
         link.addEventListener("click", (e) => {
+            let text = link.textContent;
+            console.log(text)
             if (link.getAttribute("href") === "" || link.getAttribute("href") == "#") {
                 e.preventDefault();
-                popupMessage("⚠️Pdf will available soon.")
+                popupMessage(`⚠️${text} will be available soon.`)
             }
       
         })
     })
 }
 function popupMessage(msg){
-    errorBox.textContent = msg;
-    errorBox.style.bottom = "50px";
+    errorBox.innerHTML = `<span id="errorIcon">⚠️</span>
+    <span>${msg}</span>`;
 
+    errorBox.style.bottom = "50px";
     setTimeout(() => {
         errorBox.style.bottom = "-100%";
     }, 1500);
--- assets/js/index.js
+++ assets/js/index.js
@@ -21,21 +21,25 @@ if(greetWord){
  }
   },4000)
 }
-// popBox 
+// popBox for unavailable links
 if (links && errorBox) {
     links.forEach((link) => {
         link.addEventListener("click", (e) => {
-            if (link.getAttribute("href") === "") {
-                e.preventDefault();             
-                popupMessage("⚠️Page will available soon.")
+            let text = link.textContent;
+            console.log(text)
+            if (link.getAttribute("href") === "" || link.getAttribute("href") == "#") {
+                e.preventDefault();
+                popupMessage(`⚠️${text} will be available soon.`)
             }
+      
         })
     })
 }
 function popupMessage(msg){
-    errorBox.textContent = msg;
-    errorBox.style.bottom = "50px";
+    errorBox.innerHTML = `<span id="errorIcon">⚠️</span>
+    <span>${msg}</span>`;
 
+    errorBox.style.bottom = "50px";
     setTimeout(() => {
         errorBox.style.bottom = "-100%";
     }, 1500);
